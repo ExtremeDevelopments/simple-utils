f3e78e5423a690673a92e00cd6e0738fa1c0878c
Extreme <najarvis05@gmail.com>
2021-04-20 14:21:38 -0500

    Add typings

--- src/Arrays/GetDifference.ts
+++ src/Arrays/GetDifference.ts
@@ -1,3 +1,10 @@
+/**
+ * Get the difference between 2 arrays
+ * @param {Array<any>} a Array one
+ * @param {Array<any>} b Array two
+ * @returns {Array<any>} Array values that are different
+ */
+
 export default function(a: Array<any>, b: Array<any>) {
 
   const set = new Set(b);
